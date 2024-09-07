6ff44df12c9ef0e213531669ac64018f890720df
Akito Miura <phenomer@g.hachune.net>
2024-09-07 17:11:03 +0900

    ひとまとまりで煩雑になっていたWebUIとサーバーを分割し、WebUI側のビルドツールをwebpackからviteに切り替えた

---
+++ sincromisor-client/vite.config.js
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import handlebars from 'vite-plugin-handlebars';
+
+export default defineConfig({
+    server: {
+        open: true,
+    },
+    plugins: [
+        handlebars({
+            partialDirectory: resolve(__dirname, 'src/partials')
+        })
+    ],
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                simple: resolve(__dirname, 'simple.html'),
+                single: resolve(__dirname, 'single.html'),
+                double: resolve(__dirname, 'double.html'),
+                glass: resolve(__dirname, 'glass.html')
+            }
+        }
+    }
+});
