8b4dfb03f46ff868818b91bb4fde685ab092c3aa
Akito Miura <phenomer@g.hachune.net>
2024-09-10 23:31:40 +0900

    不要な処理を削除

--- install-server-assets.sh
+++ install-server-assets.sh
@@ -6,7 +6,5 @@ set -x
 cd "$(dirname "${0}")"
 
 mkdir -p sincromisor-server/assets/3rd_party
-mkdir -p sincromisor-server/assets/3rd_party/characters
-
 curl -o "sincromisor-server/assets/3rd_party/yamnet.tflite" \
     https://storage.googleapis.com/mediapipe-models/audio_classifier/yamnet/float32/latest/yamnet.tflite
