c15ff75f82b3466bb3ed0426d31a36264a06b81b
Akito Miura <phenomer@g.hachune.net>
2024-09-23 02:57:15 +0900

    install.shで、config.ymlがないとinstall-client-toolchain.shがコケる問題を修正

--- install.sh
+++ install.sh
@@ -5,11 +5,11 @@ set -x
 
 cd "$(dirname "${0}")"
 
+if [ ! -e config.yml ]; then
+    cp examples/config.yml config.yml
+fi
+
 ./install-client-toolchain.sh
 ./install-server-toolchain.sh
 ./install-client-assets.sh
 ./install-server-assets.sh
-
-if [ ! -e config.yml ]; then
-    cp examples/config.yml config.yml
-fi
