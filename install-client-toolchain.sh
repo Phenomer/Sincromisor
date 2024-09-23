eea21bc4b2b2e29f3e87bcd083db43a0485dc877
Akito Miura <phenomer@g.hachune.net>
2024-09-23 19:01:51 +0900

    条件分岐の考慮漏れやメモリ不足などでインストールスクリプトがコケる問題を修正

--- install-client-toolchain.sh
+++ install-client-toolchain.sh
@@ -8,7 +8,11 @@ NODEJS_ARCH='linux-x64'
 
 cd "$(dirname "${0}")"
 
-if ! which npm > /dev/null; then
+if which npm > /dev/null; then
+    echo 'npm found.'
+elif [ -x "$(pwd)/node/bin/npm" ]; then
+    PATH="$(pwd)/node/bin:${PATH}"
+else
     curl -O "https://nodejs.org/dist/${NODEJS_VERSION}/node-${NODEJS_VERSION}-${NODEJS_ARCH}.tar.xz"
     tar xpf "node-${NODEJS_VERSION}-${NODEJS_ARCH}.tar.xz"
     mv "node-${NODEJS_VERSION}-${NODEJS_ARCH}" node
@@ -19,4 +23,4 @@ cd "$(dirname "${0}")"/sincromisor-client
 npm install
 mkdir -p public/mediapipe-wasm
 cp -r node_modules/@mediapipe/tasks-vision/wasm/* public/mediapipe-wasm
-npm run build
+NODE_OPTIONS="--max-old-space-size=2048" npm run build
--- install-server-toolchain.sh
+++ install-server-toolchain.sh
@@ -5,7 +5,7 @@ set -x
 
 cd "$(dirname "${0}")"
 
-if [ ! which rye > /dev/null ]; then
+if ! which rye > /dev/null; then
     curl -sSf https://rye.astral.sh/get | RYE_INSTALL_OPTION='--yes' RYE_TOOLCHAIN_VERSION=cpython@3.10.14 bash
     . "$HOME/.rye/env"
 fi
