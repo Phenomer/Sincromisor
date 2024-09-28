2aebdbe029a85e1adb8cc3755705819c5f0222ac
Akito Miura <phenomer@g.hachune.net>
2024-09-29 02:09:13 +0900

    uvは常に最新版をインストールするようにした

--- install-server-toolchain.sh
+++ install-server-toolchain.sh
@@ -6,7 +6,7 @@ set -x
 cd "$(dirname "${0}")"
 
 if ! which uv > /dev/null; then
-    curl --proto '=https' --tlsv1.2 -LsSf https://github.com/astral-sh/uv/releases/download/0.4.17/uv-installer.sh | sh
+    curl -LsSf https://astral.sh/uv/install.sh | sh
     source $HOME/.cargo/env
 fi
 uv sync
