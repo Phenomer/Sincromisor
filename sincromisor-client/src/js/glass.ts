6e09132a4f15e4aacdf35fe64d240fa435ce505a
Akito Miura <phenomer@g.hachune.net>
2024-10-09 22:40:21 +0900

    glass.htmlを開いた際にDebugConsoleが自動的に有効になるようにした

--- sincromisor-client/src/js/glass.ts
+++ sincromisor-client/src/js/glass.ts
@@ -1,9 +1,11 @@
 import { SincroScene } from "./Scene/SincroScene";
 import { SincroGlassScene } from "./Scene/SincroGlassScene";
 import { SincroInitializer } from "./SincroInitializer";
+import { DebugConsoleManager } from "./UI/DebugConsoleManager";
 
 class SincroGlassInitializer extends SincroInitializer {
     override initializeSincroScene(): SincroScene {
+        DebugConsoleManager.getManager().showDebugConsole();
         return new SincroGlassScene(this.charCanvas, this.talkManager,
             this.dialogManager.enableVR(),
             this.dialogManager.enableCharacter(),
