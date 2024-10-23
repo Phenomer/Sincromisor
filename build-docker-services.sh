63f371500ee07d09438521e9499f58f4c8ba99d8
Akito Miura <phenomer@g.hachune.net>
2024-10-23 12:38:51 +0900

    docker imageビルドスクリプトの作業ディレクトリを固定

--- build-docker-services.sh
+++ build-docker-services.sh
@@ -2,11 +2,11 @@
 
 set -e
 
+cd "$(dirname "${0}")"
+
 if [ ! -e ./Docker/env_vars/config.yml ]; then
     echo config.yml does not exist.
     exit 1
 fi
-
-exit 0
 
 docker compose --profile full-service create --build --force-recreate
