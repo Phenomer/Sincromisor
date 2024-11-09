71410722bd4778265f689a7d4a886f8f2a653251
Akito Miura <phenomer@g.hachune.net>
2024-11-09 20:35:22 +0900

    uvのインストールパス変更に対応 https://github.com/astral-sh/uv/pull/8420

--- install-server-toolchain.sh
+++ install-server-toolchain.sh
@@ -7,11 +7,13 @@ cd "$(dirname "${0}")"
 
 if ! which uv >/dev/null; then
     curl -LsSf https://astral.sh/uv/install.sh | sh
-    . "${HOME}/.cargo/env"
 fi
+
+PATH="${HOME}/.local/bin:${PATH}"
 
 uv sync \
     --group sincro-rtc \
     --group speech-extractor \
     --group speech-recognizer \
+    --group text-processor \
     --group voice-synthesizer
