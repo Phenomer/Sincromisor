e54b9343d417c4cd80595cddbfcdf8cc0aeaf43b
Akito Miura <phenomer@g.hachune.net>
2024-12-29 01:17:30 +0900

    顔がよく見えるよう、VRM用カメラの位置を調整

--- sincromisor-client/src/ts/SincroVRM/VRMScene/VRMCamera.ts
+++ sincromisor-client/src/ts/SincroVRM/VRMScene/VRMCamera.ts
@@ -6,12 +6,12 @@ export class VRMCamera {
 
     constructor(targetElement: HTMLElement) {
         const CAMERA_FOV = 30.0;
-        const CAMERA_Z = 1.5;
+        const CAMERA_Z = 1.2;
         this.camera = new PerspectiveCamera(CAMERA_FOV, window.innerWidth / window.innerHeight, 0.1, 20.0);
-        this.camera.position.set(0.0, 1.35, CAMERA_Z);
+        this.camera.position.set(0.0, 1.45, CAMERA_Z);
         const controls = new OrbitControls( this.camera, targetElement );
         controls.screenSpacePanning = true;
-        controls.target.set( 0.0, 1.3, 0.0 );
+        controls.target.set( 0.0, 1.4, 0.0 );
         controls.update();
     }
 }
